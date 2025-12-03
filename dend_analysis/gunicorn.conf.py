cfd35201f5f43b167301ad52fa0ea6019e6c8ad3
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2025-12-03 12:40:16 -0500

    Add files via upload

--- dend_analysis/gunicorn.conf.py
+++ dend_analysis/gunicorn.conf.py
@@ -2,5 +2,5 @@ import webbrowser, threading
 
 def post_fork(server, worker):
     def open_browser():
-        webbrowser.open("http://127.0.0.1:8050")
+        webbrowser.open('http://127.0.0.1:8050/DSA-2', new=0)
     threading.Timer(1.25, open_browser).start()
