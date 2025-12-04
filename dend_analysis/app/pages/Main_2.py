4e50b839421265e168614e61c6321533fe3b0e15
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2025-12-04 01:28:24 -0500

    Add files via upload

--- dend_analysis/app/pages/Main_2.py
+++ dend_analysis/app/pages/Main_2.py
@@ -27,6 +27,21 @@ def restart_apps():
         return f"Error restarting: {e}"
     
 
+def restart_appsx():
+    try:
+        # Kill existing gunicorn/app processes
+        subprocess.run(["pkill", "-f", "python"], check=False)
+
+        # Relaunch the app script directly
+        subprocess.Popen([
+            "/usr/bin/python3", "app/app.py"
+        ])
+        return "Restart triggered!"
+    except Exception as e:
+        return f"Error restarting: {e}"
+
+    
+
 name='pinn'
 page_dir=f"/dsa-{name}"
 
@@ -44,15 +59,21 @@ param = algorithm_param()
 mapp = app_run_param(param)
 def layout():
     return mapp.app_layout
+ 
 
-@callback( 
-    Input("restart-button", "n_clicks"),
-    prevent_initial_call=True
-)
-def restart_app(n_clicks):
-    if n_clicks:
-        restart_apps()
-        return "Restart requested. Server will restart shortly."
+# @callback(  
+#     Input("restart-button", "n_clicks"),
+#     prevent_initial_call=True
+# )
+# def restart_app(n_clicks):
+#     if not n_clicks:
+#         raise dash.exceptions.PreventUpdate
+
+#     # your restart logic
+#     restart_apps()
+
+#     # clear the store
+#     return  
 
 
 for gval in list(set(param['param_input']['param'])):
@@ -66,32 +87,36 @@ for gval in list(set(param['param_input']['param'])):
         if n_clicks:
             return not is_open
         return is_open
- 
- 
+    
+
 @callback(
-    mapp.Output,                             
-    Input("run-button", "n_clicks"),           
+    Output("output-file-upload", "children"),
+    Output("shared-data", "data"),   # save to store
+   [Input("upload-data", "filename"), 
+     Input("restart-button", "n_clicks")],
+     # # # Input("upload-data", "filename"), 
     [State(idx, "value") for idx in mapp.Input_id] + [
         State("upload-data", "contents"),
         State("upload-data", "filename"),
         State("upload-data", "last_modified"),
-        State("id-destination", "value") ,
-    ],  
-    prevent_initial_call=True
+        State("id-destination", "value"),
+    ],
+    prevent_initial_call=False
 )
-
-
-
-def update_output(n_clicks, *values):
-    if n_clicks == 0:
-        raise dash.exceptions.PreventUpdate
-
+def display_filenames(filenames,restart_clicks, *values):
     param=mapp.rebuild_param(values)
 
     *other_values, contents, filenames, last_modified, objs_path_org = values
     export_dir = os.path.dirname(objs_path_org)
     nam = os.path.basename(objs_path_org)
-    print('[[[[[[[[[[[[- FILE NAMES -]]]]]]]]]]]]' ,filenames)
+
+    if restart_clicks: 
+        # contents=None
+        # values=None 
+        restart_apps()
+        return html.Div("Restart requested"), {}
+    hhg=[f for f in os.listdir(objs_path_org) if os.path.isdir(os.path.join(objs_path_org, f))]
+    print('[[[[[[[[[[[[- FILE NAMES -]]]]]]]]]]]]' ,filenames,nam,export_dir,hhg)
     dend_names_chld = {} 
     # objs_path_org = os.path.join(export_dir, nam)  # Data directory, e.g. file_path_data/nam/ 
     if isinstance(contents, list):
@@ -108,26 +133,62 @@ def update_output(n_clicks, *values):
             weights=[[10., 0.81]],   # ML shaft/spine parameter: decrease the first value to increase spine vertices
         )  
  
-    else: 
-        dend_names = [f for f in os.listdir(objs_path_org) if os.path.isdir(os.path.join(objs_path_org, f))]
+    else:  
         dend_names_chld[nam] = dict(  
-            dend_names=dend_names, 
+            dend_names=[f for f in os.listdir(objs_path_org) if os.path.isdir(os.path.join(objs_path_org, f))], 
             obj_org_path=objs_path_org,
             weights=[[10., 0.81]],   # ML shaft/spine parameter: decrease the first value to increase spine vertices
-        ) 
+        )  
+ 
+    store_data = {
+        "param": param, 
+        "nam": nam, 
+        "export_dir": export_dir,
+        'dend_names_chld':dend_names_chld,
+    }
+ 
+    if contents is None:
+        display = html.Div([
+            html.H4("No files uploaded yet. Directory has:"),
+            html.Ul([html.Li(name) for name in [f for f in os.listdir(objs_path_org) if os.path.isdir(os.path.join(objs_path_org, f))]])
+        ])
+    else:
+        display = html.Div([
+            html.H4("Uploaded Files"),
+            html.H4(f"--------{param['Smooth']}"),
+            html.Ul([html.Li(name) for name in filenames])
+        ]) 
 
+    return display, store_data
+
+
+@callback(
+    mapp.Output, 
+    Input("run-button", "n_clicks"),
+    State("shared-data", "data"),
+    prevent_initial_call=True
+)
+def update_output(n_clicks, store_datas):
+    if n_clicks == 0 or not store_datas:
+        raise dash.exceptions.PreventUpdate
+    store_data=store_datas.copy()
+    store_datas={}
+    param = store_data["param"] 
+    dend_names_chld = store_data["dend_names_chld"] 
+    export_dir = store_data["export_dir"] 
+    nam = store_data["nam"] 
     gdas=get_data_all(names_dic=dend_names_chld,file_path_data=export_dir)
     dend_data = gdas.part(nam)  
-    alg=algorithm(param=param,)
-    result=alg.text(  
-                dend_data = dend_data,   
-                true_name='true_0', 
-                dnn_mode=param['dnn_mode']['param'],
-                model_type=param['path_head']['param'],
-                path_display = path_display, 
-                )
-
-    return   
+    alg=algorithm(param=param,)  
+    _ = alg.text(
+        dend_data=dend_data,
+        true_name='true_0',
+        dnn_mode=param['dnn_mode']['param'],
+        model_type=param['path_head']['param'],
+        path_display=path_display,
+    )
+     
+    return    
 
 
 
