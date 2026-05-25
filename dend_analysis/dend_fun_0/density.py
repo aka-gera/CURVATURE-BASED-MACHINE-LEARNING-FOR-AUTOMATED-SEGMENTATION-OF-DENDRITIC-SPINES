d3cb33e0b35ebb1b364ed1ff56454a72bfa8b9c1
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-05-24 23:09:16 -0400

    Add files via upload

--- dend_analysis/dend_fun_0/density.py
+++ dend_analysis/dend_fun_0/density.py
@@ -11,11 +11,11 @@ def projecton_ab(points,a,b):
 
 #################### Dendrite Density Plot ############################################# 
  
-def flatten_cylinder(points, a, b, eps_angle=0.05, eps_height=0.005, n_replicates_theta=1, n_replicates_height=1, ): 
-    # Ensure the axis vector is  
+def flatten_cylinder(points, a, b, eps_angle=0.05, eps_height=0.005, n_replicates_theta=1, n_replicates_height=1, ):  
     axis_vector = b.astype(float) - a.astype(float)
-    axis_vector /= np.linalg.norm(axis_vector)  # Normalize
-        
+    L= np.linalg.norm(axis_vector)  # Normalize
+    axis_vector /=L
+    points=np.vstack((a, points,b))
     points_on_axis = points - a 
     height_component = np.dot(points_on_axis, axis_vector)
         
@@ -28,8 +28,10 @@ def flatten_cylinder(points, a, b, eps_angle=0.05, eps_height=0.005, n_replicate
     hmin, hmax = np.min(height_component), np.max(height_component)
     height_range = hmax - hmin
      
-    flattened_points = np.vstack((angles, height_component)).T
-     
+    flattened_all = np.vstack((angles, height_component)).T
+      
+    flattened_points = flattened_all[1:-1]
+
     replicate_points = []
     grid_size_th = n_replicates_theta #int(np.sqrt(n_replicates))  # e.g. 3 for 9 replicates
     grid_size_h = n_replicates_height #int(np.sqrt(n_replicates))  # e.g. 3 for 9 replicates
@@ -46,7 +48,10 @@ def flatten_cylinder(points, a, b, eps_angle=0.05, eps_height=0.005, n_replicate
     if replicate_points:
         flattened_points = np.vstack([flattened_points] + replicate_points)
     
-    return flattened_points,np.vstack((angles, height_component)).T
+    # return flattened_points,np.vstack((angles, height_component)).T,[hmin,hmax]
+    return flattened_points,flattened_all[1:-1],[hmin,hmax]
+ 
+
 
 
 
@@ -179,7 +184,7 @@ class  get_cylinder:
         neck_pointsss=projecton_ab(neck_pointss,point_a,point_b)
         centroid_proj_cylder=Find_points_radius(r=radius,a=neck_pointsss,b=neck_pointss)  
  
-        flattened_points,flattened_points_org = flatten_cylinder(centroid_proj_cylder, 
+        flattened_points,flattened_points_org,hmm = flatten_cylinder(centroid_proj_cylder, 
                                             a=point_a, 
                                             b=point_b, 
                                             eps_angle=flat_offset,
@@ -189,10 +194,11 @@ class  get_cylinder:
                                              ) 
         heatmap, xedges, yedges = np.histogram2d(flattened_points[:,0], flattened_points[:,1], bins=bins)
         smoothed_heatmap = gaussian_filter(heatmap, sigma=sigma_gaussian_param)
-  
-        angle_mask = (xedges[:-1] >= -np.pi - eps_theta) & (xedges[:-1] <= np.pi*(1+0.04) + eps_theta)
-
-        hmin, hmax = np.min(flattened_points_org[:,1]), np.max(flattened_points_org[:,1])
+        eps_theta=0.5*np.pi
+        angle_mask = (xedges[:-1] >= -np.pi - eps_theta) & (xedges[:-1] <= np.pi*(1+0.00000004) + eps_theta)
+        # angle_mask = (xedges[:-1] >= -np.pi - eps_theta) & (xedges[:-1] <= np.pi*(1+0.04) + eps_theta)
+        # print(flattened_points_org)
+        hmin, hmax = hmm#np.min(flattened_points_org[:,1]), np.max(flattened_points_org[:,1])
         
         height_mask = (yedges[:-1] >= hmin - eps_height) & (yedges[:-1] <= hmax + eps_height)
  
@@ -217,7 +223,7 @@ class  get_cylinder:
                 title_side='right',        
                 titleside='right'            
             )  
-        xx= (flattened_points[:,0] >= -np.pi-epss) & (flattened_points[:,0] <= np.pi+epss) 
+        # xx= (flattened_points[:,0] >= -np.pi-epss) & (flattened_points[:,0] <= np.pi+epss) 
 
         yy=flattened_points[:,1]-(np.max(flattened_points[:,1]) + np.min(flattened_points[:,1])) / 2  
         yyy=flattened_points_org[:,1]-(np.max(flattened_points_org[:,1]) + np.min(flattened_points_org[:,1])) / 2 
@@ -239,10 +245,9 @@ class  get_cylinder:
 
         self.density_org_points=[]
         self.density_org_points.append(plotly_scatter(centroid_proj_cylder, marker=None, mode='markers', color='black', symbol=None, size=12, opacity=0.9, name=f'spine neck proj.', showlegend=True))
-        # self.density_org_points.extend(plotly_lines(shaft_pointss,centroid_proj_cylder,color='yellow',width=10,mode='lines',showlegend=False))
         self.density_org_points.extend(plotly_lines(neck_pointsss,centroid_proj_cylder,color='yellow',width=10,mode='lines',showlegend=False))
-        # self.density_org_points.append(plotly_scatter(projection, marker=None, mode='markers', color='red', symbol=None, size=10, opacity=0.30, name='spine proj. cylinder', showlegend=True))
-        # self.density_org_points.append(plotly_scatter(shaft_points, marker=None, mode='markers', color='blue', symbol=None, size=10, opacity=0.30, name='shaft cylinder', showlegend=True))
+        self.density_org_points.append(plotly_scatter(np.linspace(point_a, point_b, num=100) , marker=None, mode='markers', color='purple', symbol=None, size=5, opacity=0.70, name='central axis', showlegend=True))
+        '''
         self.density_org_points.append(plotly_scatter(neck_points, marker=None, mode='markers', color='black', symbol=None, size=5, opacity=0.8, name='neck org.', showlegend=True))
         self.density_org_points.append(plotly_scatter(neck_pointss, marker=None, mode='markers', color='black', symbol='x', size=5, opacity=0.99, name='neck proj.', showlegend=True))
         self.density_org_points.append(plotly_scatter(neck_pointsss, marker=None, mode='markers', color='black', symbol='square', size=5, opacity=0.99, name='neck proj. proj.', showlegend=True))
@@ -250,9 +255,12 @@ class  get_cylinder:
         self.density_org_points.append(plotly_scatter(shaft_pointss, marker=None, mode='markers', color='blue', symbol='x', size=5, opacity=0.99, name='shaft proj.', showlegend=True))
         self.density_org_points.extend(plotly_lines(neck_points,neck_pointss,color='black',width=5,mode='lines',showlegend=False))
         self.density_org_points.extend(plotly_lines(shaft_points,shaft_pointss,color='blue',width=5,mode='lines',showlegend=False))
-        self.density_org_points.append(plotly_scatter(np.linspace(point_a, point_b, num=100) , marker=None, mode='markers', color='purple', symbol=None, size=5, opacity=0.70, name='central axis', showlegend=True))
 
+        '''
 
+        # self.density_org_points.extend(plotly_lines(shaft_pointss,centroid_proj_cylder,color='yellow',width=10,mode='lines',showlegend=False))
+        # self.density_org_points.append(plotly_scatter(projection, marker=None, mode='markers', color='red', symbol=None, size=10, opacity=0.30, name='spine proj. cylinder', showlegend=True))
+        # self.density_org_points.append(plotly_scatter(shaft_points, marker=None, mode='markers', color='blue', symbol=None, size=10, opacity=0.30, name='shaft cylinder', showlegend=True))
 
         filtered_heatmap_cyl = smoothed_heatmap[np.ix_(angle_mask, height_mask)] 
         z2d = (filtered_heatmap.T)**filtered_heatmap_scale
